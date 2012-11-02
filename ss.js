f9c2f98fd0203dd9ddf5f05d23429ee358b88dcb
Paul Brannan <cout@iostream.(none)>
2012-11-02 00:33:42 -0400

    Draw a board

--- ss.js
+++ ss.js
@@ -1,3 +1,22 @@
 $(function() {
-    $( "#draggable" ).draggable();
+
+// $("#draggable").draggable();
+
+var rows = 10;
+var cols = 10;
+
+var a = [ ];
+for(var row = 0; row < rows; ++row) {
+  a.push('<tr>');
+  for(var col = 0; col < cols; ++col) {
+    a.push('<td></td>')
+  }
+  a.push('/<tr>');
+}
+s = a.join('')
+console.log(s);
+console.log($('#board'));
+$('#board').append(s);
+
 });
+
